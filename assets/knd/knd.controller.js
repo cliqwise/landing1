eb1c97c58af1ccf7d3eab011f46cf8f6cffed9ed
cliqwise <154613517+cliqwise@users.noreply.github.com>
2023-12-22 23:10:35 +0800

    Add files via upload

---
+++ assets/knd/knd.controller.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * cliqwise Theme v1.0
+ * 
+ * created by suleyman kandilci
+ * designed by can aydin
+ * 
+ * contact us: suleymankndlc@outlook.com
+ *             duzst97@gmail.com
+*/
+
+import './js/selectbox.js';
+
